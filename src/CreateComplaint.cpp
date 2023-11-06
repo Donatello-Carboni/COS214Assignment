fe2460b77244167d29cd0d318b4d028d3fb81733
Albino-aye-aye <alecwatson15@gmail.com>
2023-11-06 20:47:16 +0200

    added main  for commands pauses

--- src/CreateComplaint.cpp
+++ src/CreateComplaint.cpp
@@ -3,14 +3,30 @@
 
 Report* CreateComplaint::buildReport(int Happy)
 {
+    std::string read;
+    if(Inspection==false){
+        std::cout<<"Report Creation happening"<<std::endl;
+        std::cout<<"Press Enter to Continue"<<std::endl;
+        std::cin>>read;
+    }
      std::string head;
      std::string body;
     if(Happy>50){
+        if(this->Inspection==false){
+            std::cout<<"Happy Customer could have otherwise been an unhappy customer"<<std::endl;
+            std::cout<<"Press Enter to Continue"<<std::endl;
+            std::cin>>read;
+        }
         head="Happy Customer";
         body="Customer was happy with their order";
         return new Review(head,body,Happy);
     }
     else{
+        if(this->Inspection==false){
+            std::cout<<"Unhappy Customer could have otherwise been an happy customer"<<std::endl;
+            std::cout<<"Press Enter to Continue"<<std::endl;
+            std::cin>>read;
+        }
         head="Unhappy Customer";
         body="Customer has a complaint";
         
@@ -36,9 +52,21 @@ void CreateComplaint::setHappy(int Happy){
     this->happy=Happy;
 }
 void CreateComplaint::execute()
-{
+{   std::string read;
+    if(Inspection==false){
+        std::cout<<"Report command"<<std::endl;
+        std::cout<<"Press Enter to Continue"<<std::endl;
+        std::cin>>read;
+    }
     Manager::getManager().addReport(this->buildReport(happy));
-    std::cout<<"Report added"<<std::endl;
+    if(Inspection==false){
+        std::cout<<"Report added"<<std::endl;
+        std::cout<<"Press Enter to Continue"<<std::endl;
+        Manager::getManager().toString();
+        std::cin>>read;
+        Inspection=true;
+    }
+    
 }
 
 
